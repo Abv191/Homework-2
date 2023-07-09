c8fc4866e439861695616cc8f3385b106912e577
Abv191 <abvabv191@gmail.com>
2023-07-09 21:28:31 +0200

    V2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 
-
 def get_birthdays_per_week(users):
     today = datetime.now().date()
     next_week = today + timedelta(days=7)
@@ -11,21 +10,26 @@ def get_birthdays_per_week(users):
     for user in users:
         name = user['name']
         birthday = user['birthday'].date()
-        if today <= birthday < next_week:
-            weekday = weekdays[birthday.weekday()]
+        if today < birthday < next_week:
+            if birthday.weekday() >= 5:  # Якщо ДР випадає на суботу або неділю
+                weekday = weekdays[0]  # Переносимо на понеділок
+            else:
+                weekday = weekdays[birthday.weekday()]
             birthdays_by_day[weekday].append(name)
 
     for weekday, names in birthdays_by_day.items():
         if names:
             print(f"{weekday}: {', '.join(names)}")
 
+
 # Приклад використання
 users = [
-    {'name': 'John', 'birthday': datetime(2023, 6, 28)},
+    {'name': 'John', 'birthday': datetime(2023, 9, 4)},
     {'name': 'Alice', 'birthday': datetime(2023, 6, 30)},
     {'name': 'Bob', 'birthday': datetime(2023, 7, 2)},
     {'name': 'Kate', 'birthday': datetime(2023, 7, 3)},
-    {'name': 'Mike', 'birthday': datetime(2023, 7, 5)}
+    {'name': 'Mike', 'birthday': datetime(2023, 7, 5)},
+    {'name': 'Alex', 'birthday': datetime(2023, 8, 12)}
 ]
 
 get_birthdays_per_week(users)
